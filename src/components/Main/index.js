bc8ef80ffcad0c569b415ab0046f154e90203fe6
Alan Nascimento <alan.silva@truckpad.com.br>
2019-08-31 01:04:20 -0300

    Fetch and list repositories

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -1,27 +1,79 @@
-import React from 'react';
-import { FaGithubAlt, FaPlus } from 'react-icons/fa';
+import React, { Component } from 'react';
+import { FaGithubAlt, FaPlus, FaSpinner } from 'react-icons/fa';
 
-import { Container, Form, SubmitButton } from './styles';
+import api from '../../services/api';
 
-export default function Main() {
-  return (
-    <Container>
-      <h1>
-        <FaGithubAlt />
-        Repositories
-      </h1>
+import { Container, Form, SubmitButton, List } from './styles';
 
-      <Form onSubmit={() => { }}>
-        <input
-          type="text"
-          placeholder="Add repository"
-        />
+export default class Main extends Component {
+  state = {
+    newRepo: '',
+    repositories: [],
+    loading: false,
+  }
 
-        <SubmitButton disabled>
-          <FaPlus color="#fff" size={14}></FaPlus>
+  handleInputChange = e => {
+    this.setState({ newRepo: e.target.value });
+  }
 
-        </SubmitButton>
-      </Form>
-    </Container>
-  )
+  handleSubmit = async e => {
+    e.preventDefault();
+
+    this.setState({ loading: true })
+
+    const { newRepo, repositories } = this.state;
+
+    const response = await api.get(`/repos/${newRepo}`);
+
+    const data = {
+      name: response.data.full_name,
+    }
+
+    this.setState({
+      repositories: [...repositories, data],
+      newRepo: '',
+      loading: false,
+    })
+  }
+
+  render() {
+    const { newRepo, repositories, loading } = this.state;
+
+    return (
+
+      <Container>
+        <h1>
+          <FaGithubAlt />
+          Repositories
+        </h1>
+
+        <Form onSubmit={this.handleSubmit}>
+          <input
+            type="text"
+            placeholder="Add repository"
+            value={newRepo}
+            onChange={this.handleInputChange}
+          />
+          <SubmitButton loading={loading}>
+            {
+              loading
+                ? <FaSpinner color="#fff" size={14} />
+                : <FaPlus color="#fff" size={14} />
+            }
+          </SubmitButton>
+        </Form>
+
+        <List>
+          {repositories.map(repository => {
+            return (
+              <li key={repository.name}>
+                <span>{repository.name}</span>
+                <a href={''}>Details</a>
+              </li>)
+          })}
+        </List>
+
+      </Container>
+    )
+  }
 }
