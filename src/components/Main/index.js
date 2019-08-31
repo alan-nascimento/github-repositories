e448db501e6be918f5a4a64ba09a8519a48566f0
Alan Nascimento <alan.silva@truckpad.com.br>
2019-08-30 23:49:31 -0300

    Add styled components

---
+++ src/components/Main/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { FaGithubAlt, FaPlus } from 'react-icons/fa';
+
+import { Container, Form, SubmitButton } from './styles';
+
+export default function Main() {
+  return (
+    <Container>
+      <h1>
+        <FaGithubAlt />
+        Repositories
+      </h1>
+
+      <Form onSubmit={() => { }}>
+        <input
+          type="text"
+          placeholder="Add repository"
+        />
+
+        <SubmitButton disabled>
+          <FaPlus color="#fff" size={14}></FaPlus>
+
+        </SubmitButton>
+      </Form>
+    </Container>
+  )
+}
